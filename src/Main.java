28af1fe6e9684a9b659a94b2b9059662a588d35a
antonkupreychik <kupreychikanton@gmail.com>
2024-01-26 21:59:55 +0300

    add some changes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 
 public class Main {
     public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            int i1 = 10;
+        }
         System.out.println();
     }
 }
