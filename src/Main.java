6ffe96dbe46d5b73366c7cb1f15063b1d1c1d63c
vtb4097740 <vtb4097740@corp.dev.vtb.ru>
2024-02-02 20:22:09 +0300

    add SomeClass

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            int i1 = 10;
-        }
-        System.out.println();
+        System.out.println("hello!");
     }
 }
