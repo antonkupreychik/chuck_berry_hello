e15944dd661d2332790b793609af81664a2f5de1
vtb4097740 <vtb4097740@corp.dev.vtb.ru>
2024-02-02 20:14:11 +0300

    add SomeClass

--- src/SomeClass.java
+++ src/SomeClass.java
@@ -1,5 +1,7 @@
 public class SomeClass {
     public static void main(String[] args) {
         System.out.println("Hello from SomeClass");
+
+        System.out.println("Add antoher info");
     }
 }
